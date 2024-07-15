594ec125fbe096ead541c01fa24ae073a1840630
Dhatwalia <dhatwaliashubham3@gmail.com>
2024-07-15 10:02:55 +0530

    practice

--- index.js
+++ index.js
@@ -4,29 +4,41 @@ const URL = require("./models/url")
 const path = require("path");
 
 
+//   MOngoDB Connection ----------------------------
 
 const {connectMongoDB} = require('./connect');
 connectMongoDB('mongodb://localhost:27017/short-url')
-.then(()=> console.log("MongoDB connected"));
+    .then(() => console.log("MongoDB connected"));
 
+
+
+//   ejs view engine --------- for front end connection --------
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./view"));
 
 
+
+// Middlewares --------------------------------
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+
+//   frontend ------ Routes ---------
 const staticRouter = require('./routes/staticRouter');
 app.use('/', staticRouter);
 
+
+
+//  BAckend Routes -------------------------------
 
 const urlRoute = require('./routes/url');
 app.use("/url", urlRoute);
 
 
 
-
+//  Backend Route    ----------------           
 app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
